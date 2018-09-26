12c5988a60287dd89c321b58328ec92c00860b7f
Ken Dale <ken@kendaleiv.com>
2018-09-25 20:58:35 -0400

    1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+## Chocolatey Package: azure-data-studio
+This is a [Chocolatey](https://chocolatey.org/) package to install [SQL Operations Studio](https://docs.microsoft.com/en-us/sql/azure-data-studio).
+
+[![Chocolatey package version](https://img.shields.io/chocolatey/v/azure-data-studio.svg)](https://chocolatey.org/packages/azure-data-studio)
+[![Chocolatey package download count](https://img.shields.io/chocolatey/dt/azure-data-studio.svg)](https://chocolatey.org/packages/azure-data-studio)
+
+## Installation
+1. Install [Chocolatey](https://chocolatey.org/) *(if not already installed)*
+2. `choco install azure-data-studio`
---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
+
+$pp = Get-PackageParameters
+$mergeTasks = "!runCode"
+Write-Host "Merge Tasks: `n$mergeTasks"
+
+$packageArgs = @{
+  packageName    = 'azure-data-studio'
+  fileType       = 'EXE'
+  url64bit       = 'https://go.microsoft.com/fwlink/?linkid=2024683'
+
+  softwareName   = 'Azure Data Studio'
+
+  checksum64     = 'B2142AB3F22A20E43C3B9FA0DB791CCCABFC956B9ECAE7E65D410BC8248DA658'
+  checksumType64 = 'sha256'
+
+  silentArgs     = "/verysilent /suppressmsgboxes /mergetasks=""$mergeTasks"" /log=""$env:temp\azure-data-studio.log"""
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
