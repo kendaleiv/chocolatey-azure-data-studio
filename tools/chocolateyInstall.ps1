877818fea2fbf6397a9d4f679c2c873ea5d440b9
Brendon Thiede <brendon.thiede@gmail.com>
2019-03-25 22:16:08 -0400

    Updating SHA256 checksum to match version 1.5.2
    Version 1.5.2 was released on 3/22/2019. New checksum was grabbed from PowerShell via `(Get-FileHash .\azuredatastudio-windows-user-setup-1.5.2.exe -Algorithm SHA256).Hash`

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $packageArgs = @{
 
   softwareName   = 'Azure Data Studio'
 
-  checksum64     = '153BF594089CBC51D675702C4E4B575D8F71B75D2958FB456D6B4BC1576E18B8'
+  checksum64     = 'CE02B5B0B9D7A500E5D5DB1790290237C830B10A5990FCFFF9B7E7FCE56BEC34'
   checksumType64 = 'sha256'
 
   silentArgs     = "/verysilent /suppressmsgboxes /mergetasks=""$mergeTasks"" /log=""$env:temp\azure-data-studio.log"""
