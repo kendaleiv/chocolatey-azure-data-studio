dcc4c4f49133a9624af4ea2d64d893b5dc693c1f
Ken Dale <ken@kendaleiv.com>
2019-05-13 17:27:18 -0400

    1.7.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Write-Host "Merge Tasks: `n$mergeTasks"
 $packageArgs = @{
   packageName    = 'azure-data-studio'
   fileType       = 'EXE'
-  url64bit       = 'https://go.microsoft.com/fwlink/?linkid=2087317'
+  url64bit       = 'https://go.microsoft.com/fwlink/?linkid=2091491'
 
   softwareName   = 'Azure Data Studio'
 
-  checksum64     = 'FE4BE9A0520B576941B8624E41F8AD9AC8BC1B294C8122314DAD997BE254232D'
+  checksum64     = 'CC8CD53752EDDDAD5A41630F7D0041695FFFF82E51963C99079A8219DF450FB3'
   checksumType64 = 'sha256'
 
   silentArgs     = "/verysilent /suppressmsgboxes /mergetasks=""$mergeTasks"" /log=""$env:temp\azure-data-studio.log"""
