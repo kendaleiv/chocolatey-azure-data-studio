486b7f3784e6d8b540c9dbc1896ed7b1b093c0f5
sonofhammer <daniil.marchenko@gmail.com>
2019-10-08 15:34:50 -0500

    1.12.1
    Use direct azure blob storage link to avoid issue #5

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Write-Host "Merge Tasks: `n$mergeTasks"
 $packageArgs = @{
   packageName    = 'azure-data-studio'
   fileType       = 'EXE'
-  url64bit       = 'https://go.microsoft.com/fwlink/?linkid=2105134'
+  url64bit       = 'https://azuredatastudiobuilds.blob.core.windows.net/releases/1.12.1/azuredatastudio-windows-setup-1.12.1.exe'
 
   softwareName   = 'Azure Data Studio'
 
-  checksum64     = 'BA43790B60C06D3290027140FEC159279E9B919C2864E63EB1134D0818DAC644'
+  checksum64     = '103581E77C54F82E6BCECC592FE959541AB2DED7B9591B941C2AC4AA460CCB1E'
   checksumType64 = 'sha256'
 
   silentArgs     = "/verysilent /suppressmsgboxes /mergetasks=""$mergeTasks"" /log=""$env:temp\azure-data-studio.log"""
