2622c5d58920904d5c6a1315791afe23240d2275
Ken Dale <ken@kendaleiv.com>
2020-12-11 09:50:27 -0500

    1.25.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -8,11 +8,11 @@ Write-Host "Merge Tasks: `n$mergeTasks"
 $packageArgs = @{
   packageName    = 'azure-data-studio'
   fileType       = 'EXE'
-  url64bit       = 'https://sqlopsbuilds.azureedge.net/stable/eb3c0f5d9df86b9490606aab7fffaa253b0aca4d/azuredatastudio-windows-setup-1.24.0.exe'
+  url64bit       = 'https://sqlopsbuilds.azureedge.net/stable/e7eacc32c0d20bd4da843320c3bfac231b308c98/azuredatastudio-windows-setup-1.25.1.exe'
 
   softwareName   = 'Azure Data Studio'
 
-  checksum64     = '29436EC6EE8AA4E5721B801B6A0658DD5CD37AA404ACD2AED09142E56DD00824'
+  checksum64     = 'AA5DCC04EE4CD130EEFD82F94B57CBBBBEF511A3FF6AAD44C9B0D066102B8115'
   checksumType64 = 'sha256'
 
   silentArgs     = "/verysilent /suppressmsgboxes /mergetasks=""$mergeTasks"" /log=""$env:temp\azure-data-studio.log"""
